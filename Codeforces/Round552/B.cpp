25bd9d091a1bb5110f924ce15af6e3d3c337bf11
Ashish Lalchandani <37297441+Ashish110110@users.noreply.github.com>
2019-10-03 00:25:00 +0530

    Create B.cpp

---
+++ Codeforces/Round552/B.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+ 
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	for (int i = 0; i < n; ++i) {
+		cin >> a[i];
+	}
+	sort(a.begin(), a.end());
+	a.resize(distance(a.begin(),unique(a.begin(),a.end())));
+	if (a.size() > 3) {
+		cout << -1 << endl;
+	} else {
+		if (a.size() == 1) {
+			cout << 0 << endl;
+		} else if (a.size() == 2) {
+			if ((a[1] - a[0]) % 2 == 0) {
+				cout << (a[1] - a[0]) / 2 << endl;
+			} else {
+				cout << a[1] - a[0] << endl;
+			}
+		} else {
+			if (a[1] - a[0] != a[2] - a[1]) {
+				cout << -1 << endl;
+			} else {
+				cout << a[1] - a[0] << endl;
+			}
+		}
+	}
+ 
+	return 0;
+}
